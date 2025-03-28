03de2bc79b65f5b2a88b581bd1dfd0e1dfa7614f
Arif Hakim <arif.hakim@gopay.co.id>
2025-03-28 09:03:58 +0700

    feat: adding allure reports

--- conftest.py
+++ conftest.py
@@ -3,27 +3,25 @@ from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 from dotenv import load_dotenv
 import os
+import allure
 
 load_dotenv()
 saucedemo_url = os.getenv('SAUCE_DEMO_URL')
 
 @pytest.fixture
 def setup():
-  # init setup part
-  chrome_option = ChromeOptions()
-  chrome_option.add_experimental_option('detach', True)
-  chrome_option.add_argument("--headless=new")
+  with allure.step('init & setup option chrome driver'):
+    chrome_option = ChromeOptions()
+    chrome_option.add_experimental_option('detach', True)
+    chrome_option.add_argument("--headless=new")
+    driver = webdriver.Chrome(options= chrome_option)
+    driver.implicitly_wait(5) # can be general stuff for waiting time boundaries
+    driver.maximize_window()
 
-  # init chrome driver
-  driver = webdriver.Chrome(options= chrome_option)
-  driver.implicitly_wait(5) # can be general stuff for waiting time boundaries
-  driver.maximize_window()
+  with allure.step('go to url'):
+    driver.get(saucedemo_url)
 
-  # go to url
-  driver.get(saucedemo_url)
-
-  # yield driver
   yield driver
 
-  # tear down
-  driver.quit()
+  with allure.step('tear down'):
+    driver.quit()
--- tests/test_login_sauce_demo.py
+++ tests/test_login_sauce_demo.py
@@ -4,6 +4,7 @@ from pages.login import Login
 from pages.products import Products
 from dotenv import load_dotenv
 import os
+import allure
 
 login_data = DataProvider.login_data
 load_dotenv()
@@ -15,35 +16,52 @@ valid_password = os.getenv('VALID_PASSWORD')
 
 @pytest.mark.positiveTc
 @pytest.mark.loginCase
+@allure.title("Test login feature")
+@allure.description("This test is for covering login feature | positive case")
+@allure.testcase("testrail-12345")
 def test_login_web(setup):
   login_page = Login(setup)
   product_page = Products(setup)
   
-  # login web-app
-  login_page.fill_username(valid_username)
-  login_page.fill_pasword(valid_password)
-  login_page.click_login_button()
-
-  current_url = setup.current_url
-  product_page.check_current_tag_line()
-  product_page.check_cart_icon()
-  len_products = product_page.get_count_product()
+  with allure.step("Input username"):
+    login_page.fill_username(valid_username)
   
-  assert current_url == saucedemo_products_url
-  assert len_products > 0
+  with allure.step("Input password"):
+    login_page.fill_pasword(valid_password)
+  
+  with allure.step("Click login button"):
+    login_page.click_login_button()
+
+  with allure.step("Validate assertions"):
+    current_url = setup.current_url
+    product_page.check_current_tag_line()
+    product_page.check_cart_icon()
+    len_products = product_page.get_count_product()
+
+    assert current_url == saucedemo_products_url
+    assert len_products > 0
+    setup.save_screenshot('success_login_screenshot.png')
+    allure.attach.file("success_login_screenshot.png", name="Screenshot Login", attachment_type=allure.attachment_type.PNG)
 
 @pytest.mark.negativeTc
 @pytest.mark.loginCase
 @pytest.mark.parametrize('username, password, error_message', login_data)
+@allure.title("Test login feature - negative test case")
+@allure.description("This test is for covering login feature | negative case")
+@allure.testcase("testrail-12346")
 def test_negative_case_login_web(username, password, error_message, setup):
   login_page = Login(setup)
 
   # login web-app
-  login_page.fill_username(username)
-  login_page.fill_pasword(password)
-  login_page.click_login_button()
+  with allure.step("Input username"):
+    login_page.fill_username(username)
   
-  # assert err message
-  login_page.validate_error_message(error_message)
+  with allure.step("Input password"):
+    login_page.fill_pasword(password)
   
-  assert setup.current_url == saucedemo_url
+  with allure.step("Click login button"):
+    login_page.click_login_button()
+  
+  with allure.step("Validate assertions"):
+    login_page.validate_error_message(error_message)
+    assert setup.current_url == saucedemo_url
