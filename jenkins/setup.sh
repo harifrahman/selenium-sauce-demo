b569c9e18993ebbce4aeffdf8e2c91614676aaa8
Arif Hakim <arif.hakim@gopay.co.id>
2025-03-28 15:37:52 +0700

    feat: adjust setup for docker and jenkins script

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+set -e  # Exit on error
+
+echo "Starting Jenkins at $(date)"
+
 # Start Jenkins
-exec /usr/bin/tini -- /usr/local/bin/jenkins.sh
+exec /usr/bin/tini -- /usr/local/bin/jenkins.sh || {
+    echo "Failed to start Jenkins"
+    exit 1
+}
